6c1f88c0e105cb8803da1797ebae025eab2e1237
NongHoi <nonghoi2004@gmail.com>
2025-03-23 00:54:31 +0700

    Hoi

--- ShoeShop/Controllers/HomeController.cs
+++ ShoeShop/Controllers/HomeController.cs
@@ -1,31 +1,45 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using ShoeShop.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
 
-namespace ShoeShop.Controllers;
-
-public class HomeController : Controller
+namespace ShoeShop.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly NikeShopDbContext _context;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, NikeShopDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products
+                .Where(p => p.ProductSizes.Any(ps => ps.Stock > 0)) // Chỉ hiển thị sản phẩm có size còn hàng
+                .Include(p => p.ProductImages) // Load hình ảnh
+                .OrderByDescending(p => p.CreatedAt)
+                .Take(12)
+                .ToListAsync();
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
