186b7421c2aadcf591207cd8c0db4940e6ff6500
NongHoi <nonghoi2004@gmail.com>
2025-03-22 16:13:09 +0700

    hoi

---
+++ ShoeShop2/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoeShop.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public int CategoryID { get; set; }
+        public Category? Category { get; set; }
+
+        [Required, StringLength(255)]
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        [Required, Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public ICollection<ProductSize> ProductSizes { get; set; } = new List<ProductSize>();
+        public ICollection<ProductImage> ProductImages { get; set; } = new List<ProductImage>();
+
+        //public ICollection<ProductImage> ProductImages { get; set; }
+        //public ICollection<ProductSize> ProductSizes { get; set; }
+    }
+}
