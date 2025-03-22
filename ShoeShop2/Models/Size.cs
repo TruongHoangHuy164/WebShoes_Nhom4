07bb8a159c884c1975e50d28adb9314e653c2113
Lehoangf <danglehoang.k22@gmail.com>
2025-03-22 16:18:14 +0700

    Hoang

---
+++ ShoeShop2/Models/Size.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoeShop.Models
+{
+    public class Size
+    {
+        public int SizeID { get; set; }
+
+        [Required, StringLength(10)]
+        public string SizeName { get; set; }
+    }
+}
